b097fd6d2b31ca3cd84cb8c9957680e6b065635a
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2023-05-04 13:06:13 +0100

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 
 @NgModule({
   declarations: [
