7cabe31bea9deb38dabc54b5cb45f3f567d53f81
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2023-05-04 20:34:55 +0100

    Login created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { LoginComponent } from './Pages/login/login.component';
+import { SignupComponent } from './Pages/signup/signup.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    LoginComponent,
+    AppComponent,
+    SignupComponent,
+    
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ SignupComponent, LoginComponent ]
 })
 export class AppModule { }
