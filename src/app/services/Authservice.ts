c117b24c452d8857f00e4e5d37382bdec3bde174
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2023-05-07 01:25:03 +0100

    Authservice comment

--- src/app/services/Authservice.ts
+++ src/app/services/Authservice.ts
@@ -1,16 +1,17 @@
+//This typescript is to store the state of user  login
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  isLoggedIn = false;
- 
+  isLoggedIn = false; // Default value of isLoggedIn is false
 
   constructor() {}
 
+  // Method to set the value of isLoggedIn and store it in localStorage
   setLoggedIn(value: boolean) {
     this.isLoggedIn = value;
-     localStorage.setItem('isLoggedIn', value ? 'true' : 'false');
+    localStorage.setItem('isLoggedIn', value ? 'true' : 'false'); // Store isLoggedIn as a string 'true' or 'false' in localStorage
   }
 }
