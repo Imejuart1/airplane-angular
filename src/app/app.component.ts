8a569573db49f5d5be9317c8798246098b40c8ab
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2023-05-07 01:35:45 +0100

    add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,35 @@
+// Import necessary modules and services
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/Authservice'
 
+// Declare the component metadata
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  // Inject the AuthService into the component
   constructor(private authService: AuthService) {}
 
+  // Initialize the component on load
   ngOnInit() {
+    // Get the logged-in status from local storage
     const isLoggedIn = localStorage.getItem('isLoggedIn');
     if (isLoggedIn) {
+      // Set the logged-in status in the AuthService based on local storage value
       this.authService.setLoggedIn(isLoggedIn === 'true');
     }
   }
+
+  // Check if the user is logged in or not
   get isLoggedIn() {
     return this.authService.isLoggedIn;
   }
-      handleLogout() {
-  this.authService.setLoggedIn(false);
-}
 
+  // Handle the user logout event
+  handleLogout() {
+    // Set the logged-in status in the AuthService to false
+    this.authService.setLoggedIn(false);
+  }
 }
