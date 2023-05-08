2fa08a086b4616a3e7a479cc447acb98d481e338
Ikwuegbuenyi Augustus Meju <101446446+Imejuart1@users.noreply.github.com>
2023-05-08 15:41:12 +0100

    hide my keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,10 @@
 export const environment = {
   production: false,
-  firebase: {
-  apiKey: "AIzaSyDHp5maA9CFj4UkXe_xzlEiRtt0HxKfHss",
-  authDomain: "pipureport-6b980.firebaseapp.com",
-  projectId: "pipureport-6b980",
-  storageBucket: "pipureport-6b980.appspot.com",
-  messagingSenderId: "246680606543",
-  appId: "1:246680606543:web:d518f2534cbfd5b45a173a"
-  }
+  firebaseConfig: {
+  apiKey: " ",
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " "
 };
