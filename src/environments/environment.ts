4d2f5d15089f0110a64a248cf0481617b0f7834f
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2023-05-20 21:32:26 +0100

    new

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,11 @@
 export const environment = {
   production: false,
-  firebaseConfig: {
-  apiKey: " ",
-  authDomain: " ",
-  projectId: " ",
-  storageBucket: " ",
-  messagingSenderId: " ",
-  appId: " "
+  firebase: {
+  apiKey: "AIzaSyDHp5maA9CFj4UkXe_xzlEiRtt0HxKfHss",
+  authDomain: "pipureport-6b980.firebaseapp.com",
+  projectId: "pipureport-6b980",
+  storageBucket: "pipureport-6b980.appspot.com",
+  messagingSenderId: "246680606543",
+  appId: "1:246680606543:web:d518f2534cbfd5b45a173a"
+  }
 };
